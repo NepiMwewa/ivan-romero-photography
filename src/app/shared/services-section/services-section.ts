79efd9c73bde49eee05524f568fc81fdae27bcf1
Alexander Harmon <wolfaceking@gmail.com>
2025-12-22 14:55:18 +0000

    Added styles for the Services section

--- src/app/shared/services-section/services-section.ts
+++ src/app/shared/services-section/services-section.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ServiceBox } from "../service-box/service-box";
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-services-section',
-  imports: [ServiceBox],
+  imports: [ServiceBox, NgOptimizedImage],
   templateUrl: './services-section.html',
   styleUrl: './services-section.css',
 })
